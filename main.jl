c2cc573e545d872d1d423e130c6b673374b9366f
Diligence Dev <diligence.dev@web.de>
2025-01-01 13:59:19 +0100

    read in preferences from csv

--- main.jl
+++ main.jl
@@ -1,13 +1,56 @@
+using CSV
+using DataFrames
 using JuMP
 using SCIP
 
-# Define the sets
-players = 1:48
-slots = 1:3
-cubes = 1:20
+rank_to_score = [100, 70, 50, 10, 5]
 
-# Define the parameters
-score = rand(length(players), length(cubes))  # Replace with actual score data
+# read in preferences.csv as dataframe
+preferences = CSV.read("preferences.csv", DataFrame)
+preferences = preferences[1:16, :]
+
+player_mails = unique(preferences[!, 2])
+# TODO fill with players who did not fill survey
+cube_names = filter(.!ismissing, unique([preferences[!, 3];
+                     preferences[!, 4];
+                     preferences[!, 5];
+                     preferences[!, 6];
+                     preferences[!, 7]]))
+
+function pid(player_mail)
+    return findfirst(player_mail .== player_mails)
+end
+
+function cid(cube_name)
+    return findfirst(cube_name .== cube_names)
+end
+
+# TODO preferences JOS = HEIKE
+
+# Define the sets
+players = 1:length(player_mails)
+slots = 1:3
+cubes = 1:length(cube_names)
+
+# Define the parameters (=scores)
+score = zeros(length(players), length(cubes))
+
+for pmail in preferences[!, 2]
+    p = pid(pmail)
+    picks = [preferences[preferences[!, 2] .== player_mails[p], i][1] for i in 3:7]
+    unique!(picks)
+    filter!(x -> !ismissing(x), picks)
+
+    for i in eachindex(rank_to_score)
+        if i > length(picks)
+            for cube in setdiff(cubes, cid.(picks))
+                score[p, cube] = rank_to_score[i]
+            end
+            break
+        end
+        score[p, cid(picks[i])] = rank_to_score[i]
+    end
+end
 
 # Create the model
 model = Model(SCIP.Optimizer)
@@ -21,30 +64,26 @@ model = Model(SCIP.Optimizer)
 @constraint(model, [p in players, s in slots], sum(pcs[p, c, s] for c in cubes) == 1)
 @constraint(model, [p in players, c in cubes], sum(pcs[p, c, s] for s in slots) <= 1)
 
+# TODO prevent Heike and Jos from playing at the same table
+# TODO check how much worse total score is with this constraint
+# @constraint(model, [c in cubes, s in slots], pcs[HEIKE, c, s] + pcs[JOS, c, s] <= 1)
+
 # Define the objective
 @objective(model, Max, sum(pcs[p, c, s] * score[p, c] for p in players, c in cubes, s in slots))
 
 # Solve the model
 optimize!(model)
 
-# Check the status of the solution
-if termination_status(model) == MOI.OPTIMAL
-    println("Optimal solution found")
-    println("Objective value: ", objective_value(model))
-    
-else
-    println("No optimal solution found")
-end
-
+# Print solution
 for slot in slots
     total_cubes = 0
     for cube in cubes
         if round(value(cs[cube, slot])) == 1
-            println("\nslot $slot Cube $cube")
+            println("\nslot $slot Cube ", cube_names[cube])
             total = 0
             for player in players
                 if round(value(pcs[player, cube, slot])) == 1
-                    println("Player $player")
+                    println("Player ", player_mails[player])
                     total += 1
                 end
             end
@@ -55,11 +94,9 @@ for slot in slots
     @assert total_cubes == length(players) / 8
 end
 
-for cube in cubes
-    println("\nCube $cube")
-    for slot in slots
-        if round(value(cs[cube, slot])) == 1
-            println("Slot $slot")
-        end
-    end
+if termination_status(model) == MOI.OPTIMAL
+    println("Optimal solution found")
+    println("Objective value: ", objective_value(model))
+else
+    println("No optimal solution found")
 end
